b78cccf627ed239a94043d199ae18eeced8dcc52
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-08-02 23:10:08 +0300

    fixed issues in skeleton section 4

--- template-parts/front-page/content-section4.php
+++ template-parts/front-page/content-section4.php
@@ -4,7 +4,7 @@
  */
 $post_id = get_the_ID();
 
-$title = demo_theme_get_acf_field('section4_title', $post_id, __('Get Reading Flow™ For Your Website Today', 'demo-theme'));
+$title = demo_theme_get_acf_field('section4_title', $post_id, __('Get Reading Flow™<br> For Your Website Today', 'demo-theme'));
 $subtitle = demo_theme_get_acf_field('section4_subtitle', $post_id, __('Read more, Engage Longer', 'demo-theme'));
 $text = demo_theme_get_acf_field('section4_text', $post_id, __('The benefits of having Reading Flow™ on your website are many. Not only does it make your content accessible to a wider audience, but it also keeps readers engaged for longer. When combined with the AI-Powered UserWay Accessibility, your site becomes more readable, compliant, and overall more accessible — even as new content and pages are added to your site.', 'demo-theme'));
 $button_link = demo_theme_get_acf_field('section4_button_link', $post_id, __('https://google.com', 'demo-theme'));
@@ -14,8 +14,8 @@ $button_link = demo_theme_get_acf_field('section4_button_link', $post_id, __('ht
     <div class="section__title section-4__title"><?php echo $title; ?></div>
     <div class="section__two-colons-row section-4__two-colons-row">
         <div class="section__colon-left section-4__colon-left">
-            <div class="section__subtitle section-1__colon-title"><?php echo $subtitle; ?></div>
-            <div class="section__subtitle section-4__text"><?php echo apply_filters('the_content', $text); ?></div>
+            <div class="section__colon-title section-1__colon-title"><?php echo $subtitle; ?></div>
+            <div class="section__colon-text section-4__colon-ßtext"><?php echo apply_filters('the_content', $text); ?></div>
             <div class="section__button section-4__button">
                 <span><?php _e('Get Reading Flow™', 'demo-theme'); ?></span>
                 <a class="section__btn section-4__btn" rel="nofollow" target="_blank" href="<?php esc_attr_e($button_link); ?>"><?php _e('Start Free Trial', 'domo-theme');?></a>
