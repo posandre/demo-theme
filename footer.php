eceeac366d1dbe27bd2ba93980de2de7d153ee30
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-08-02 14:46:30 +0300

    corrected header and footer

--- footer.php
+++ footer.php
@@ -11,24 +11,10 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'demo-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo-theme' ), 'demo-theme', '<a href="https://www.linkedin.com/in/andrii-postoliuk-88b037155/">Andrii Postoliuk</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class="demo-theme__footer"></footer>
+</div>
 
 <?php wp_footer(); ?>
 
-</body>
-</html>
+</body><!-- #body -->
+</html><!-- #html -->
