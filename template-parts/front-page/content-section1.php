fb1637ee250d88f26f4127d87609f0a7f11750a0
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-08-02 20:19:27 +0300

    corrected section 1 template

--- template-parts/front-page/content-section1.php
+++ template-parts/front-page/content-section1.php
@@ -5,15 +5,15 @@
 
 $post_id = get_the_ID();
 
-$title = demo_theme_get_acf_field('section1_title', $post_id, __('Reading Flow™', 'demo-theme'));
+$title = demo_theme_get_acf_field('section1_title', $post_id, __('Reading Flow<span>™</span>', 'demo-theme'));
 $subtitle = demo_theme_get_acf_field('section1_subtitle', $post_id, __('A patented technology that improves reading speed and accuracy. <span class="marker">Now on UserWay</span>.', 'demo-theme'));
 $text = demo_theme_get_acf_field('section1_text', $post_id, __('UserWay is proud to partner with Beeline Reader to offer <strong>Reading Flow™</strong>, a patented text display innovation that makes reading on-screen texts easier, faster, and more enjoyable.', 'demo-theme'));
 $video_link = demo_theme_get_acf_field('section1_video_link', $post_id, __('https://google.com', 'demo-theme'));
 $button_link = demo_theme_get_acf_field('section1_button_link', $post_id, __('https://google.com', 'demo-theme'));
 $bottom_title = demo_theme_get_acf_field('section1_bottom_title', $post_id, __('Breeze Through Long Texts', 'demo-theme'));
-$bottom_text = demo_theme_get_acf_field('section1_bottom_text', $post_id, __('<strong>Reading Flow™</strong> works by using a simple cognitive trick — an eye-guiding color gradient — to pull your eyes through long blocks of text, helping improve focus, concentrating and reading speed.
-The Intel-backed technology has won awards from the United Nations and Stanford University, and has been adopted by top US colleges and universities. The technology has also received widespread recognition by Scientific American, the United Nations and Google who hail BeeLine for its benefits to people with ADHD, Dyslexia and as a general tool to i<strong>mprove reading speed & comprehension</strong> for everyone.
-<strong>Reading Flow™</strong> is the latest addition to the best-in-class accessibility functions offered by UserWay.', 'demo-theme'));
+$bottom_text = demo_theme_get_acf_field('section1_bottom_text', $post_id, __('<p><strong>Reading Flow™</strong> works by using a simple cognitive trick — an eye-guiding color gradient — to pull your eyes through long blocks of text, helping improve focus, concentrating and reading speed.</p>
+<p>The Intel-backed technology has won awards from the United Nations and Stanford University, and has been adopted by top US colleges and universities. The technology has also received widespread recognition by Scientific American, the United Nations and Google who hail BeeLine for its benefits to people with ADHD, Dyslexia and as a general tool to i<strong>mprove reading speed & comprehension</strong> for everyone.</p>
+<p><strong>Reading Flow™</strong> is the latest addition to the best-in-class accessibility functions offered by UserWay.</p>', 'demo-theme'));
 
 ?>
 
@@ -22,7 +22,10 @@ The Intel-backed technology has won awards from the United Nations and Stanford 
     <div class="section__subtitle section-1__subtitle"><?php echo $subtitle; ?></div>
     <div class="section__subtitle section-1__text"><?php echo apply_filters('the_content', $text); ?></div>
     <div class="section__video-button section-1__button">
-        <a class="section__video-link section-1__link" rel="nofollow" target="_blank" href="<?php esc_attr_e($button_link); ?>"><?php _e('Watch the video', 'demo-theme'); ?></a>
+        <a class="section__video-link section-1__link" rel="nofollow" target="_blank" href="<?php esc_attr_e($button_link); ?>">
+            <img width="72" height="72" alt="<?php esc_attr_e('Play buimage', 'demo-theme');?>" src="<?php echo demo_theme_get_image_url('video-play-back.svg'); ?>">
+            <?php _e('Watch the video', 'demo-theme'); ?>
+        </a>
     </div>
     <div class="section__button section-1__button">
         <span><?php _e('Get Reading Flow™', 'demo-theme'); ?></span>
